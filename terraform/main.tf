354a7f5dfaedb415a434e07a4005b8fd1a8f3fad
mercynwaka <nwakamercy@gmail.com>
2026-01-29 12:55:15 +0000

    Added s3 bucket for backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "backend-prod-my-state-storage"
+    key    = "production/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
