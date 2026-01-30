4cd6857df25d0b80da171c2d514430fa595e002d
mercynwaka <nwakamercy@gmail.com>
2026-01-30 19:26:48 +0000

    modified destroy file

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -119,6 +119,7 @@ resource "aws_s3_bucket_policy" "allow_cloudfront" {
 resource "aws_ecr_repository" "app_repo" {
   name                 = "${var.environment}-backend-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
